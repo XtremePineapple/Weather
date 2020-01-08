9e10d322b84fae94e9495bf85789aadcc6ed5e1e
XtremePineapple <95henryX@gmail.com>
2020-01-07 18:46:26 -0800

    yoyo

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello Bitxh!")
+console.log("Hello Boyle!")
+
+const APIpre = "http://api.openweathermap.org/data/2.5"
+const APIkey = "&appid=b3e34d17911306f4e4996155270e0bdd"
+
+const weath = axios.get(APIpre + "/weather?zip=93401,us" + APIkey)
+        .then(response => response.data)
+        .then(data => console.log(data ))
